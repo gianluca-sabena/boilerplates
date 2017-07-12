fbe8844af36f3a3417060cf7b23ad2c85ba43b68
Gianluca Sabena <gianluca.sabena@unicredit.eu>
2017-07-12 18:36:05 +0200

    minor

--- python/hello-world-cli/hello_world_cli.py
+++ python/hello-world-cli/hello_world_cli.py
@@ -6,35 +6,49 @@ from __future__ import print_function
 import argparse
 import sys
 
-def hello(msg):
-    """docs string"""
-    print('Hello, {0}!'.format(msg.name))
 
+class App:
+    """a class"""
 
-def goodbye(msg):
-    """docs string"""
-    print('Goodbye, {0}!'.format(msg.name))
+    config = {
+        "key": {
+            "key-nested": "value"
+        }
+    }
+
+    def __init__(self):
+        self.data = []
+        parser = argparse.ArgumentParser()
+
+        subparsers = parser.add_subparsers()
+
+        hello_parser = subparsers.add_parser('hello')
+        hello_parser.add_argument('name')  # add the name argument
+        # set the default function to hello
+        hello_parser.set_defaults(func=self.hello)
+
+        goodbye_parser = subparsers.add_parser('goodbye')
+        goodbye_parser.add_argument('name')
+        goodbye_parser.set_defaults(func=self.goodbye)
+        print("Hello, world! {0}".format(self.config["key"]))
+        args = parser.parse_args()
+        if len(sys.argv) == 1:
+            parser.print_help()
+            sys.exit(1)
+        args.func(args)  # call the default function
+
+    def hello(self, msg):
+        """docs string"""
+        print('Hello, {0}!'.format(msg.name))
+
+    def goodbye(self, msg):
+        """docs string"""
+        print('Goodbye, {0}!'.format(msg.name))
 
 
 def main():
     """Main"""
-    parser = argparse.ArgumentParser()
-
-    subparsers = parser.add_subparsers()
-
-    hello_parser = subparsers.add_parser('hello')
-    hello_parser.add_argument('name')  # add the name argument
-    hello_parser.set_defaults(func=hello)  # set the default function to hello
-
-    goodbye_parser = subparsers.add_parser('goodbye')
-    goodbye_parser.add_argument('name')
-    goodbye_parser.set_defaults(func=goodbye)
-    print("Hello, world!")
-    args = parser.parse_args()
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-    args.func(args)  # call the default function
+    App()
 
 if __name__ == '__main__':
     main()
