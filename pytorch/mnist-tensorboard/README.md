3f78d58e43828e8dd09b1941611adc474fad3b16
Gianluca <gianluca.sabena@gmail.com>
2019-10-04 18:27:59 +0200

    minor

---
+++ pytorch/mnist-tensorboard/README.md
@@ -0,0 +1,4 @@
+# Pytorch MNIST tensorboard
+
+- Run simple `pipenv run python src/mnist-tensorboard-simple.py`
+- Run full example `pipenv run python src/mnist-tensorboard-full.py`
--- pytorch/mnist-tensorboard/src/mnist-tensorboard-simple.py
+++ pytorch/mnist-tensorboard/src/mnist-tensorboard-simple.py
@@ -7,7 +7,7 @@ from torchvision import datasets, transforms
 writer = SummaryWriter()
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-trainset = datasets.MNIST('mnist_train', train=True, download=True, transform=transform)
+trainset = datasets.MNIST('data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 model = torchvision.models.resnet50(False)
 # Have ResNet model take in grayscale rather than RGB
